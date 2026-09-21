383b5c5263ecedeb3ffe3bef59e41ca059fde8ca
Nekrasov Aleksey <137201708+showman228@users.noreply.github.com>
2026-09-21 16:43:49 +0300

    Add: valid-sudoku - submission-3

---
+++ Data Structures & Algorithms/valid-sudoku/submission-3.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+
+        # number: count_of_number
+        row = defaultdict(set) 
+        col = defaultdict(set)
+        box = defaultdict(set)
+
+        for r in range(len(board)):
+            for c in range(len(board)):
+                if board[r][c] == '.':
+                    continue
+
+                if (board[r][c] in row[r]
+                    or board[r][c] in col[c] 
+                    or board[r][c] in box[(r//3, c//3)]):
+                    return False
+
+                row[r].add(board[r][c])
+                col[c].add(board[r][c])
+                box[(r//3, c//3)].add(board[r][c])
+    
+
+        return True
+            
+
